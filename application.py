ea25f29b40dc1c4fa4e9da19ad51f280810655e8
Cat Scheer <catherine.rymer@gmail.com>
2017-11-16 07:07:56 -0700

    Make a start at the function, template, and form to add new items to a PodList. See #3

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, ThoughtPod, PodItem
@@ -21,6 +21,18 @@ def podList(pod_id):
     thoughtpod = session.query(ThoughtPod).filter_by(id=pod_id).one()
     items = session.query(PodItem).filter_by(thought_pod_id=thoughtpod.id)
     return render_template('podlist.html', thoughtpod=thoughtpod, items=items)
+
+
+@app.route('/pods/<int:pod_id>/new/', methods=['GET', 'POST'])
+def newPodListItem(pod_id):
+    if request.method == 'POST':
+        newItem = PodItem(
+            title=request.form['title'], thought_pod_id=pod_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('podList', pod_id=pod_id))
+    else:
+        return render_template('newpodlistitem.html', pod_id=pod_id)
 
 if __name__ == '__main__':
     app.debug = True
