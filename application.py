69daa3c8e4a27dd355576d114b1f1fdecaa6ce53
Cat Scheer <catherine.rymer@gmail.com>
2017-11-16 07:55:15 -0700

    Get the edit function working and add an edit item template. Closes #4

--- application.py
+++ application.py
@@ -16,6 +16,7 @@ session = DBSession()
 def HelloWorld():
     return "Hello World"
 
+
 @app.route('/pods/<int:pod_id>/')
 def podList(pod_id):
     thoughtpod = session.query(ThoughtPod).filter_by(id=pod_id).one()
@@ -33,6 +34,33 @@ def newPodListItem(pod_id):
         return redirect(url_for('podList', pod_id=pod_id))
     else:
         return render_template('newpodlistitem.html', pod_id=pod_id)
+
+@app.route('/pods/<int:pod_id>/<int:item_id>/edit/', methods=['GET', 'POST'])
+def editPodListItem(pod_id, item_id):
+    editedItem = session.query(PodItem).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        if request.form['title']:
+            editedItem.title = request.form['title']
+        if request.form['url']:
+            editedItem.url = request.form['url']
+        if request.form['description']:
+            editedItem.description = request.form['description']
+        if request.form['time_investment']:
+            editedItem.time_investment = request.form['time_investment']
+        if request.form['difficulty_level']:
+            editedItem.difficulty_level = request.form['difficulty_level']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('podList', pod_id=pod_id))
+    else:
+        return render_template(
+            'editpodlistitem.html', pod_id=pod_id, item_id=item_id, item=editedItem)
+
+
+@app.route('/pods/<int:pod_id>/<int:item_id>/delete/')
+def deletePodListItem(pod_id, item_id):
+    return "page to delete an existing PodList item"
+
 
 if __name__ == '__main__':
     app.debug = True
