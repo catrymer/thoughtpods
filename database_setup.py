338932a0107eab28ce0531d986b6dddc6b8057b8
Cat Scheer <catherine.rymer@gmail.com>
2017-11-22 11:26:55 -0700

    Update code for PEP8 compliance. See #12

--- database_setup.py
+++ database_setup.py
@@ -8,6 +8,7 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
 
@@ -15,51 +16,53 @@ class User(Base):
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
 
+
 class ThoughtPod(Base):
 
     __tablename__ = 'thought_pod'
 
-    pod_title = Column(String(80),nullable = False)
+    pod_title = Column(String(80), nullable=False)
     description = Column(String(250))
-    id = Column(Integer,primary_key = True)
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'pod_title'    : self.pod_title,
-           'description'  : self.description,
-           'id'           : self.id,
-       }
+        """Return object data in easily serializeable format"""
+        return {
+            'pod_title': self.pod_title,
+            'description': self.description,
+            'id': self.id,
+        }
+
 
 class PodItem(Base):
 
     __tablename__ = 'pod_item'
 
-    title = Column(String(80),nullable = False)
-    id = Column(Integer, primary_key = True)
+    title = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     url = Column(String(250))
     description = Column(String(250))
     time_investment = Column(String(80))
     difficulty_level = Column(Integer)
-    thought_pod_id = Column(Integer,ForeignKey('thought_pod.id'))
+    thought_pod_id = Column(Integer, ForeignKey('thought_pod.id'))
     thought_pod = relationship(ThoughtPod)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'title'            : self.title,
-           'url'              : self.url,
-           'description'      : self.description,
-           'time_investment'  : self.time_investment,
-           'difficulty_level' : self.difficulty_level,
-           'id'               : self.id,
-       }
+        """Return object data in easily serializeable format"""
+        return {
+            'title': self.title,
+            'url': self.url,
+            'description': self.description,
+            'time_investment': self.time_investment,
+            'difficulty_level': self.difficulty_level,
+            'id': self.id,
+        }
 
 
 engine = create_engine('sqlite:///thoughtpods.db')
